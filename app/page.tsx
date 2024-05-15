74395f11fb0629683060a65bd994e59b7bc82ef1
sundaray <pgcim14.hemant@spjimr.org>
2024-05-15 22:36:17 +0530

    added the text

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="flex-1 container max-w-3xl space-y-8">
+    <main className="flex-1 container max-w-3xl space-y-8 my-24">
       <section>
         <h2>Can you introduce yourself?</h2>
         <p className="mb-4">
@@ -31,12 +31,13 @@ export default function Home() {
         <p className="mb-4">
           So far, I&apos;ve developed the following three AI projects:
         </p>
-        <ol className="list-decimal ml-8">
+        <ol className="list-decimal ml-8 mb-4 text-neutral-700">
           <li>
             <a
               href="https://www.yapprz.com/"
               target="_blank"
               rel="noopener noreferrer"
+              className="text-blue-600 underline hover:no-underline"
             >
               yapprz.com
             </a>
@@ -48,6 +49,7 @@ export default function Home() {
               href="https://www.excelai.pro/"
               target="_blank"
               rel="noopener noreferrer"
+              className="text-blue-600 underline hover:no-underline"
             >
               excelai.pro
             </a>
@@ -59,12 +61,152 @@ export default function Home() {
               href="https://www.makeaudio.app/"
               target="_blank"
               rel="noopener noreferrer"
+              className="text-blue-600 underline hover:no-underline"
             >
               makeaudio.app
             </a>
             : Convert text to audio
           </li>
         </ol>
+        <p>
+          Through these projects, I gained a deep understanding of how to use
+          various OpenAI APIs, including chat completion, text to speech, and
+          vision.
+        </p>
+      </section>
+      <section>
+        <h2>
+          What experience do you have using AI tools to automate tasks and
+          workflows? Can you provide examples of how you&apos;ve done this
+          successfully?
+        </h2>
+        <p className="mb-4">
+          I have extensive experience using AI tools to automate tasks and
+          streamline workflows, particularly in the areas of coding, research,
+          and writing. When ChatGPT was first launched, I had just started
+          working as a freelance technical writer for an edtech platform. I
+          quickly recognized the potential of AI to improve my productivity, so
+          I began using ChatGPT to assist with research and drafting blog posts.
+          While other writers were outputting 2-3 1500-word posts per month, I
+          was writing 10. I had increased my productivity by 5 times.
+        </p>
+        <p className="mb-4">
+          Over the course of several months, from December 2022 to July 2023, I
+          deepened my expertise with ChatGPT, initially using the 3.5 model and
+          later upgrading to GPT-4. Through hands-on experience, I developed a
+          strong understanding of how to write effective prompts to get the best
+          responses from the AI. I learned to work around its limitations and
+          play to its strengths, which enabled me to consistently leverage the
+          tool to produce high-quality work.
+        </p>
+        <p className="mb-4">
+          As my freelance projects slowed down, I joined the AI bandwagon and
+          started building personal projects, primarily AI wrappers. Once again,
+          I relied on ChatGPT to support me with coding and troubleshooting.{" "}
+        </p>
+        <p className="mb-4">
+          When Anthropic launched Claude, I was impressed by its writing
+          capabilities and its willingness to follow instructions. I now use
+          Claude 3 Opus extensively for coding, research and writing tasks,
+          turning to GPT-4 as a complementary tool when Claude is unable to
+          help.
+        </p>
+        <p className="mb-4">
+          In addition to ChatGPT and Claude, I&apos;ve also explored Gemini Pro,
+          but found that it didn&apos;t consistently deliver the same level of
+          quality and insight.
+        </p>
+        <p className="mb-4">
+          Throughout my journey with AI tools, I&apos;ve learned to quickly
+          assess the strengths and weaknesses of each tool. While I have my
+          go-to favourites in ChatGPT and Claude, I&apos;m confident in my
+          ability to rapidly get up to speed on any AI tools that would be
+          helpful in supporting your needs as an assistant.
+        </p>
+      </section>
+      <section>
+        <h2>Where are you located?</h2>
+        <p>I&apos;m located in India. </p>
+      </section>
+      <section>
+        <h2>Does that have a good crossover with UK time?</h2>
+        <p className="mb-4">
+          India Standard Time (IST) is 4 hours and 30 minutes ahead of the UK.
+          Let&apos;s consider a typical UK workday of 8 hours from 10 AM to 7
+          PM, with a one-hour lunch break. Here&apos;s how that translates to my
+          time zone:
+        </p>
+        <ul className="list-disc ml-8 mb-4 leading-7 text-neutral-700">
+          <li>When it&apos;s 10 AM in the UK, it&apos;s 2:30 PM for me.</li>
+          <li>Your 7 PM end of day is 11:30 PM my time.</li>
+        </ul>
+        <p className="mb-4">
+          So for the majority of your workday,I&apos;m awake and available.
+          I&apos;m confident that my location won&apos;t be an obstacle, and
+          I&apos;m committed to being responsive and accessible during the core
+          hours that matter most for your workflow.
+        </p>
+      </section>
+      <section>
+        <h2>
+          Are you aware what kind of tasks you will be doing as an assistant?
+        </h2>
+        <p className="mb-4">
+          Yes, I carefully reviewed the task list you shared on X, and I have a
+          clear understanding of what the role entails. From what I gathered,
+          some of the key responsibilities include:
+        </p>
+        <ul className="list-disc ml-8 mb-4 leading-7 text-neutral-700">
+          <li>
+            Conducting research on various topics and preparing
+            briefings/summaries.
+          </li>
+          <li>
+            Drafting and editing written content like blog posts, newsletters,
+            social media posts.
+          </li>
+          <li>
+            Managing Ben&apos;s calendar, scheduling appointments and meetings.
+          </li>
+          <li>Handling email correspondence and customer inquiries.</li>
+          <li>
+            Assisting with data entry, light bookkeeping and other
+            administrative tasks as needed.
+          </li>
+          <li>
+            Helping keep projects and initiatives on track by monitoring
+            progress.
+          </li>
+          <li>
+            Providing general support to Ben and the team across a range of
+            areas.
+          </li>
+        </ul>
+      </section>
+      <section>
+        <h2>Why are you perfect for this job?</h2>
+        <p className="mb-4">
+          I believe I&apos;m an excellent fit for this role for several reasons.
+          First and foremost, I&apos;m a heavy AI user, which is one of the core
+          requirements for this position. As I&apos;ve shared, I&apos;ve used AI
+          in various ways to enhance my productivity in writing and coding. This
+          hands-on experience has given me a deep understanding of how to
+          effectively leverage AI tools to drive efficiency and deliver
+          high-quality work.
+        </p>
+        <p>
+          Moreover, my background as a technical writer aligns perfectly with
+          the role&apos;s emphasis on drafting and editing different types of
+          written content. You can check out my technical writing via this link{" "}
+          <a
+            href="https://www.makeaudio.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-600 underline hover:no-underline"
+          >
+            https://www.hemantasundaray.com/technical-writing
+          </a>
+        </p>
       </section>
     </main>
   );
